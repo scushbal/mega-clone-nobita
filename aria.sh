d3b301a60560c528cad257e548b027a46b467a40
NOBITA <88440061+nobita-o@users.noreply.github.com>
2021-08-16 20:16:30 +0600

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,6 +1,6 @@
 aria2c --enable-rpc --check-certificate=false \
-   --max-connection-per-server=10 --rpc-max-request-size=1024M \
-   --bt-stop-timeout=1200 --min-split-size=10M --follow-torrent=mem --split=10 \
+   --max-connection-per-server=5 --rpc-max-request-size=1024M \
+   --bt-stop-timeout=500 --min-split-size=10M --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=0 \
    --max-overall-upload-limit=1K --max-concurrent-downloads=15 --continue=true \
    --peer-id-prefix=-qB4360- --user-agent=qBittorrent/4.3.6 --peer-agent=qBittorrent/4.3.6 \
