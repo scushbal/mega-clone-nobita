798269345bc280497809943f72155bf7d4788c87
Theodore-xw4886 <89044682+Theodore-xw4886@users.noreply.github.com>
2021-08-19 01:18:21 +0600

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -143,7 +143,6 @@ def get_readable_message():
                     except:
                         pass
                     msg += f'\n<b>🙎 User:</b> <a href="tg://user?id={download.message.from_user.id}">{download.message.from_user.first_name}</a>'
-                    msg += f' (<a href="tg://user?id={download.message.from_user.id}">{download.message.from_user.id}</a>)'
                     msg += f"\n<b>To ❌ Stop:</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
                 msg += "\n\n"
                 if STATUS_LIMIT is not None:
