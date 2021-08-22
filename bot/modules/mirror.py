3b788b8704954f98324c339fb8a8f5e23909d3bc
Theodore-xwwx4886 <89044682+Theodore-xwwx4886@users.noreply.github.com>
2021-08-22 23:43:36 +0600

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -144,7 +144,7 @@ class MirrorListener(listeners.MirrorListeners):
 
     def onUploadComplete(self, link: str, size, files, folders, typ):
         with download_dict_lock:
-            msg = f'<b>🔰 Filename: </b><code>{download_dict[self.uid].name()}</code>\n<b>Size: </b><code>{size}</code>'
+            msg = f'<b>🔰 Filename: </b><code>{download_dict[self.uid].name()}</code>\n<b>🧭 Size: </b><code>{size}</code>'
             if os.path.isdir(f'{DOWNLOAD_DIR}/{self.uid}/{download_dict[self.uid].name()}'):
                 msg += '\n<b>🌿 Type: </b><code>Folder</code>'
                 msg += f'\n<b>🚥 SubFolders: </b><code>{folders}</code>'
